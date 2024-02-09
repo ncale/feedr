96c601628323ed69a13d94b88f990f44440bd223
ncale <nicholasbrodeur@outlook.com>
2024-02-09 14:31:20 -0600

    add neynar client

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,4 +1,5 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-export const MONGO_URI = process.env.MONGO_URI;
+export const MONGO_URI = process.env.MONGO_URI as string;
+export const NEYNAR_API_KEY = process.env.NEYNAR_API_KEY as string;
--- src/utils/mongo.ts
+++ src/utils/mongo.ts
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 import { MONGO_URI } from './config'
 
-export default function runMongo(mongoUri: string = MONGO_URI!) {
+export default function runMongo(mongoUri: string = MONGO_URI) {
     return mongoose.connect(mongoUri);
 };
