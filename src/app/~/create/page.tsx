3e71d5b18fbf9e513e096d813e1a5d61cbe124a5
ncale <nicholasbrodeur@outlook.com>
2024-02-10 14:28:42 -0600

    add client side api functions for creating a feed

--- src/app/~/create/page.tsx
+++ src/app/~/create/page.tsx
@@ -1,7 +1,43 @@
+'use client';
+
 export default function CreatePage() {
+
+  // Function to submit a new feed
+  async function postFeed() {
+    try {
+      const apiPath = `/api`;
+      const res = fetch(apiPath, {
+        method: "POST",
+        headers: {"Content-type": "application/json"},
+        body: JSON.stringify({  })
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  // Function to edit an existing feed... WIP
+  async function putFeed() {
+    try {
+      const apiPath = `/api`;
+      const res = fetch(apiPath, {
+        method: "PUT",
+        headers: {"Content-type": "application/json"},
+        body: JSON.stringify({  })
+      });  
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  function handleClick() {
+    console.log("hello");
+  };
+
   return (
     <main className="">
       <h1>C R E A T E page</h1>
+      <button onClick={handleClick}>click here to create a new feed</button>
     </main>
   );
 }
